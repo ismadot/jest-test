a9612b46e045b11ae02476157aaf6fdcf4df19b9
Adel Gutierrez <agutierrez@qin.cl>
2019-10-18 16:31:11 -0300

    more test

--- conver.js
+++ conver.js
@@ -1,31 +1,38 @@
+function convert(str){
+  const parse = str.split(" ");
+  const number = parseFloat(parse[0]);
+  const unit = parse[1];
+  const convertion = unitObj(number, unit);
+  return convertion
+}
 
-function unit(esca, unit){
-  let result = {kb:0, mb:0, gb:0, tb:0}
+function unitObj(esca, unit='') {
+  const result = {kb: 0, mb: 0, gb: 0, tb: 0}
 
-  switch(unit.toUpperCase()){
+  switch(unit.toUpperCase()) {
     case 'KB':
-      result.kb = esca
-      result.mb = result.kb/1024;
-      result.gb = result.mb/1024;
-      result.tb = result.gb/1024;
+      result.kb = esca;
+      result.mb = result.kb / 1024;
+      result.gb = result.mb / 1024;
+      result.tb = result.gb / 1024;
       break;
     case 'MB':
-      result.mb = esca
-      result.kb = result.mb*1024
-      result.gb = result.mb/1024;
-      result.tb = result.gb/1024;
+      result.mb = esca;
+      result.kb = result.mb * 1024;
+      result.gb = result.mb / 1024;
+      result.tb = result.gb / 1024;
       break;
     case 'GB':
       result.gb = esca;
-      result.mb = result.gb*1024
-      result.kb = result.mb*1024
-      result.tb = result.gb/1024;
+      result.mb = result.gb * 1024;
+      result.kb = result.mb * 1024;
+      result.tb = result.gb / 1024;
       break;
     case 'TB':
       result.tb = esca;
-      result.gb = result.tb*1024;
-      result.mb = result.gb*1024
-      result.kb = result.mb*1024
+      result.gb = result.tb * 1024;
+      result.mb = result.gb * 1024;
+      result.kb = result.mb * 1024;
       break;
     default:
       return 'la unidad representada no se encuentra entre los casos';
@@ -35,4 +42,4 @@ function unit(esca, unit){
 }
 
 
-module.exports = unit;
+module.exports = {unitObj, convert};
--- conver.test.js
+++ conver.test.js
@@ -1,21 +1,34 @@
-const unit = require('./conver');
+const {unitObj, convert} = require('./conver');
+// const convert = require('./conver');
 
 test('kb to mb,gb,tb', () => {
-  expect(unit(1024, 'kb')).toStrictEqual({ kb: 1024, mb: 1, gb: 0.0009765625, tb: 9.5367431640625e-7 });
+  expect(unitObj(1024, 'kb')).toStrictEqual({ kb: 1024, mb: 1, gb: 0.0009765625, tb: 9.5367431640625e-7 });
 });
 
 test('mb to kb,gb,tb', () => {
-  expect(unit(1024, 'MB')).toStrictEqual({ kb:1048576, mb: 1024, gb: 1, tb: 0.0009765625 });
+  expect(unitObj(1024, 'MB')).toStrictEqual({ kb:1048576, mb: 1024, gb: 1, tb: 0.0009765625 });
 });
 
 test('gb to mb,kb,tb', () => {
-  expect(unit(1, 'GB')).toStrictEqual({ kb: 1048576, mb: 1024, gb: 1, tb: 0.0009765625 });
+  expect(unitObj(1, 'GB')).toStrictEqual({ kb: 1048576, mb: 1024, gb: 1, tb: 0.0009765625 });
 });
 
 test('gb to mb,kb,tb', () => {
-  expect(unit(1, 'TB')).toStrictEqual({ kb: 1073741824, mb: 1048576, gb: 1024, tb: 1});
+  expect(unitObj(1, 'TB')).toStrictEqual({ kb: 1073741824, mb: 1048576, gb: 1024, tb: 1});
 });
 
 test('error convert ', () => {
-  expect(unit(1, 'ecta')).toBe('la unidad representada no se encuentra entre los casos');
+  expect(unitObj(1, 'ecta')).toBe('la unidad representada no se encuentra entre los casos');
+});
+
+test('convert split 1 GB', () => {
+  expect(convert('1 GB')).toStrictEqual({ kb: 1048576, mb: 1024, gb: 1, tb: 0.0009765625 });
+});
+
+test('convert split 700 MB', () => {
+  expect(convert('700 MB')).toStrictEqual({ kb: 716800, mb: 700, gb: 0.68359375, tb: 0.000667572021484375 });
+});
+
+test('convert split erro', () => {
+  expect(convert('700 aw')).toBe('la unidad representada no se encuentra entre los casos');
 });
